20b303e013209649fc2ff65e92434e4fe7e373a3
Dawid Pałubiak <dawid.palubiak@gmail.com>
2021-01-09 17:24:07 +0100

    Lines are remembered

--- los.asm
+++ los.asm
@@ -3,11 +3,18 @@ assume	CS:program, DS:dane, SS:stosik
 program	segment
 
 start:
+    MOV AX, dane
+	MOV DS, AX
+
+	MOV AX, stosik
+	MOV SS, AX
+
+	MOV SP, offset szczyt
 
 	;LOSOWANIE
 	MOV AH, 00h  ; interrupts to get system time
     INT 1AH      ; CX:DX now hold number of clock ticks since midnight
-
+	
     MOV  AX, DX
     XOR  DX, DX
     MOV  BX, 25
@@ -16,12 +23,34 @@ start:
 	XOR BX, BX
 	MOV BL, DL
 	MOV AL, losuj[BX]
-    ADD los, AL
+    ADD nr_linii, AL
     MOV AH, 02h
 	INT 21h
 	
+	MOV AL, 160
+	MUL nr_linii
+	MOV poczatek, AX
+	; MOV AH, 09h
+	; INT 21h
+
+	; MOV DX, AX
+	; MOV AH, 09h
+	; INT 21h
+
 	;PAMIETAJ LINIĘ
+	CLD
+	MOV CX,80
+	PUSH DS
+	PUSH DS
+	POP ES
+	MOV AX, 0B800h
+	MOV DS,AX
+	MOV DI, OFFSET bufor
+	MOV SI, poczatek
+	REP MOVSW
+	POP DS
 	
+	;POKOLORUJ
 
 	;SLEEP ~1S
 		MOV CX,16
@@ -33,9 +62,10 @@ skok:	PUSH CX
 		POP CX
 		LOOP skok
 
-	MOV DL,los
-	INT 02h
-	INT 21h
+
+		; mov dx, sign
+        ; mov ah, 09h
+        ; int 21h
 
 koniec: MOV AL,0h
 		MOV AH,4Ch                                        ;koniec programu
@@ -44,17 +74,28 @@ koniec: MOV AL,0h
 program	ends
 
 dane	segment
-
+	
+	; bgkolor 00h, 01h, 02h, 03h, 04h, 05h, 06h, 07h
+	; fgkolor 00h, 01h, 02h, 03h, 04h, 05h, 06h, 07h, 08h, 09h, 0ah, 0bh, 0ch, 0dh, 0eh, 0fh
+	; znak equ bgkolor[0502h]
+	sign equ 0520h
 	bufor db 160 dup(?)
-	los db 2 
+	nr_linii db 2 
 	losuj db 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 
+	poczatek dw 16
 
 dane	ends
 
 stosik	segment
 
-
+			dw 100h dup(0)
+szczyt		dw 0
 
 stosik	ends
 
 end	start
+
+;         mov dx, sign
+;         mov ah, 09h
+;         int 21h
+
