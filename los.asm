80b2b2fa128b3419099ad34a44b3336b98286f56
Dawid Pałubiak <dawid.palubiak@gmail.com>
2021-01-10 13:59:58 +0100

    Works return of line, but the wrong line xD

--- los.asm
+++ los.asm
@@ -30,12 +30,7 @@ start:
 	MOV AL, 160
 	MUL nr_linii
 	MOV poczatek, AX
-	; MOV AH, 09h
-	; INT 21h
-
-	; MOV DX, AX
-	; MOV AH, 09h
-	; INT 21h
+	
 
 	;PAMIETAJ LINIĘ
 	CLD
@@ -58,14 +53,14 @@ start:
 	INT 10h
 
 	;POKOLORUJ
-	mov cx,80   ;ile razy wypisać dany znak
+	MOV cx,80   ;ile razy wypisać dany znak
 	petla: 
-		mov ah,9   ;nr funkcji przerwania 10h
-		mov al,219   ;kod ASCII danego znaku
-		mov bh,0   ;strona video (zazwyczaj 0)
-		mov bl,0ah   ;nr koloru
+		MOV AH,9   ;nr funkcji przerwania 10h
+		MOV AL,219   ;kod ASCII danego znaku
+		MOV BH,0   ;strona video (zazwyczaj 0)
+		MOV BL,09h   ;nr koloru
 		int 10h   ;wywołanie przerwania nr 10h
-	loop petla
+	LOOP petla
 
 	;SLEEP ~1S
 		MOV CX,16
@@ -77,10 +72,25 @@ skok:	PUSH CX
 		POP CX
 		LOOP skok
 
+	;USTAW KURSOR NA WYLOSOWANĄ LINIĘ
+	MOV AH, 02h
+	MOV BH, 0
+	MOV DH, nr_linii
+	MOV DL, 0
+	INT 10h
 
-		; mov dx, sign
-        ; mov ah, 09h
-        ; int 21h
+	;PRZYWRÓC LINIĘ
+	CLD
+	MOV CX,80
+	PUSH DS
+	PUSH DS
+	POP ES
+	MOV AX, 0B800h
+	MOV ES,AX
+	MOV SI, OFFSET bufor
+	MOV DI, poczatek
+	REP MOVSW
+	POP ES
 
 koniec: MOV AL,0h
 		MOV AH,4Ch                                        ;koniec programu
@@ -90,11 +100,6 @@ program	ends
 
 dane	segment
 	
-	; bgkolor 00h, 01h, 02h, 03h, 04h, 05h, 06h, 07h
-	; fgkolor 00h, 01h, 02h, 03h, 04h, 05h, 06h, 07h, 08h, 09h, 0ah, 0bh, 0ch, 0dh, 0eh, 0fh
-	; znak equ bgkolor[0502h]
-	kolor equ 0Ah
-	sign equ 0520h
 	bufor db 160 dup(?)
 	nr_linii db 2 
 	losuj db 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 
